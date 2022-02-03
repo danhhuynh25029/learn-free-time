6661d1139f8d0246284dd7ce15a41952961445c3
danhhuynh25029 <danhhuynh250@gmail.com>
2022-02-03 21:41:27 +0700

    update

--- scala/BinarySearch.scala
+++ scala/BinarySearch.scala
@@ -8,9 +8,9 @@ object BinarySearch{
     var l = 0 ;
     var r = arr.length;
     while(l < r){
-     var tmp = (l+r)/2;
-     if(arr(tmp) > k) r = tmp - 1;
-     else l = tmp + 1;
+      var tmp = (l+r)/2;
+      if(arr(tmp) > k) r = tmp - 1;
+      else l = tmp + 1;
     }
     return true;
   }
---
+++ scala/ISO8601.scala
@@ -0,0 +1,24 @@
+object ISO8601{
+    def main(args:Array[String]){
+        val date = args(0).split("T");
+        val time = date(1).split('+');
+        val arr_date = date(0).split("-");
+        val arr_time = time(0).split(":");
+        for(i <- (arr_date.length - 1) to 0 by -1 ){
+            val o = i match{
+                case 0 => "Year : " + arr_date(i)
+                case 1 => "Month : " + arr_date(i)
+                case 2 => "Day : " + arr_date(i)
+            }
+            println(o);
+        }
+        for(i <- 0 to (arr_time.length - 1)){
+            val o = i match{
+                case 0 => "Hour : " + arr_time(i)
+                case 1 => "Minutes : " + arr_time(i)
+                case 2 => "Second : " + arr_time(i)
+            }
+            println(o);
+        }
+    }
+}
